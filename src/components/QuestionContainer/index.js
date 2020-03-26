ba77e4e740b52d786980e8378cf182d707681b12
Stephanie Fernandes <steph3001@gmail.com>
2020-03-26 03:26:58 -0300

    Update QuestionContainer component

--- src/components/QuestionContainer/index.js
+++ src/components/QuestionContainer/index.js
@@ -6,25 +6,23 @@ import './style.scss';
 
 const QuestionContainer = () => {
   return (
-    <div>
-      <section className="question__container">
-        <div className="question__top">
-          <p>Questão 1</p>
-          <Difficulty />
-        </div>
-        <p className="question__description">
-          Sobre a conhecida Idade dos Metais, na transição entre a Pré-História
-          e a História, é possível afirmar que
-        </p>
-        <div className="question__options">
-          <QuestionOption />
-          <QuestionOption />
-          <QuestionOption />
-          <QuestionOption />
-        </div>
-        <Button text="Responder" />
-      </section>
-    </div>
+    <section className="question__container">
+      <div className="question__top">
+        <p>Questão 1</p>
+        <Difficulty level="Difícil" />
+      </div>
+      <p className="question__description">
+        Sobre a conhecida Idade dos Metais, na transição entre a Pré-História e
+        a História, é possível afirmar que
+      </p>
+      <div className="question__options">
+        <QuestionOption />
+        <QuestionOption />
+        <QuestionOption />
+        <QuestionOption />
+      </div>
+      <Button text="Responder" isDisabled />
+    </section>
   );
 };
 
