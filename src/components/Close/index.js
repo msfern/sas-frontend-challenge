2f168817939c4454a22ba7415c4e26539aaf2d2b
Stephanie Fernandes <steph3001@gmail.com>
2020-03-26 03:26:09 -0300

    Create Close component

---
+++ src/components/Close/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.scss';
+
+const Close = () => {
+  return (
+    <div className="close-question">
+      <img src="../../assets/images/x-circle.svg" alt="Close" />
+      <span>Fechar</span>
+    </div>
+  );
+};
+
+export default Close;
