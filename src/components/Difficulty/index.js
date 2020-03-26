f708be1d6a60321c31b0e7ac375161ec1a509368
Stephanie Fernandes <steph3001@gmail.com>
2020-03-26 03:26:29 -0300

    Create Difficulty component

---
+++ src/components/Difficulty/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.scss';
+
+const Difficulty = () => {
+  return (
+    <div className="difficulty-indicator">
+      <img src="../../assets/images/star.svg" alt="star" />
+      <img src="../../assets/images/star.svg" alt="star" />
+      <img src="../../assets/images/star.svg" alt="star" />
+      <span>Difícil</span>
+    </div>
+  );
+};
+
+export default Difficulty;
