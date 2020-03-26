9ec5fb9d9806934b542e49ee6688ae3aab0d360e
Stephanie Fernandes <steph3001@gmail.com>
2020-03-26 02:26:19 -0300

    Update QuestionOption component

---
+++ src/components/QuestionOption/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.scss';
+
+const QuestionOption = () => {
+  return (
+    <div className="question__option">
+      não existe ligação entre o uso dos metais e a formação de grandes impérios
+    </div>
+  );
+};
+
+export default QuestionOption;
