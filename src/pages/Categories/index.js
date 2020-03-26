f0995abd76d6f0339d0bf5dbf11ce02357e90d01
Stephanie Fernandes <steph3001@gmail.com>
2020-03-26 01:09:54 -0300

    Update Categories page

--- src/pages/Categories/index.js
+++ src/pages/Categories/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import Category from '../../components/Category/index';
+import './style.scss';
 
 const Categories = () => {
-  return <div>categories</div>;
+  return (
+    <div className="categories">
+      <h1>Categorias</h1>
+      <section className="categories-list">
+        <Category />
+        <Category />
+        <Category />
+        <Category />
+        <Category />
+        <Category />
+      </section>
+    </div>
+  );
 };
 
 export default Categories;
