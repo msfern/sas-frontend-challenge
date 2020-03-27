e0a36a0bd3406442559304be29f824a159beda68
Stephanie Fernandes <steph3001@gmail.com>
2020-03-27 13:06:19 -0300

    Update Not Found page

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './style.scss';
 
 const NotFound = () => {
-  return <div>Oops, page not found</div>;
+  return <div className="not-found">Oops, page not found</div>;
 };
 
 export default NotFound;
