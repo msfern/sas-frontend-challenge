5f427479686d3cfcbac847301be9ddaad93373e9
Stephanie Fernandes <steph3001@gmail.com>
2020-03-29 15:09:19 -0300

    Update routes

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,19 +1,46 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Categories from '../pages/Categories/index';
 import Question from '../pages/Question/index';
 import Result from '../pages/Result/index';
 import NotFound from '../pages/NotFound/index';
 
-const Router = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={Categories} />
-      <Route path="/category/:categoryName/result" component={Result} />
-      <Route path="/category/:categoryName" component={Question} />
-      <Route component={NotFound} />
-    </Switch>
-  </BrowserRouter>
+const Router = (props) => (
+  <Switch>
+    <Route
+      exact
+      path="/"
+      render={() => (
+        <Categories
+          categories={props.categories}
+          goToCategory={props.goToCategory}
+        />
+      )}
+    />
+    <Route
+      path="/category/:categoryName/result"
+      render={() => (
+        <Result
+          clearQuestions={props.clearQuestions}
+          report={props.report}
+          currentCategoryId={props.currentCategoryId}
+        />
+      )}
+    />
+    <Route
+      path="/category/:categoryName"
+      render={() => (
+        <Question
+          questions={props.questions}
+          currentCategory={props.currentCategory}
+          clearQuestions={props.clearQuestions}
+          addCategoryResultsToReport={props.addCategoryResultsToReport}
+          quitQuiz={props.quitQuiz}
+        />
+      )}
+    />
+    <Route component={NotFound} />
+  </Switch>
 );
 
 export default Router;
