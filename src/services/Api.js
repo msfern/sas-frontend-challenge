4d2eb8de79bd22b99e37e4ba99c0a59aa5d09d31
Stephanie Fernandes <steph3001@gmail.com>
2020-03-29 15:09:52 -0300

    Update API call

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,7 +1,16 @@
 import axios from 'axios';
 
-export const api = axios.create({
-  baseURL: 'https://opentdb.com/api.php',
-});
+const getToken = async () => {
+  const token = await axios
+    .get('https://opentdb.com/api_token.php?command=request')
+    .then((result) => result.data.token);
+  return token;
+};
+
+const api = async () => {
+  const token = await getToken();
+  const url = `https://opentdb.com/api.php?token=${token}&encode=url3986`;
+  return url;
+};
 
 export default api;
